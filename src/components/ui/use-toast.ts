ce988eaa2bac40f5d3cfec1c671d8a87d3569330
wynwnata <akun.bait.pro@gmail.com>
2025-06-30 02:17:25 +0800

    update

--- src/components/ui/use-toast.ts
+++
@@ -1,6 +0,0 @@
-
-import { useToast as useToastOriginal, toast as toastOriginal } from "@/hooks/use-toast";
-
-// Re-export the hooks for easier import
-export const useToast = useToastOriginal;
-export const toast = toastOriginal;
